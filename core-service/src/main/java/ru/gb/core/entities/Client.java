a20a6fdaebe058237037535c3c9e186dc50ad3c8
Sergei Afonin <s.afonin@bankffin.ru>
2022-07-14 16:02:29 +0300

    Added frontend part for AccountController

--- core-service/src/main/java/ru/gb/core/entities/Client.java
+++ core-service/src/main/java/ru/gb/core/entities/Client.java
@@ -1,9 +1,6 @@
 package ru.gb.core.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -18,5 +15,12 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
     @SequenceGenerator(name = "client_gen", sequenceName = "clients_id_seq", allocationSize = 1)
     private Long id;
+
+    /**
+     * Уникальный логин клиента
+     */
+    @NonNull
+    @Column(name = "username")
+    private String username;
 
 }
--- core-service/src/main/java/ru/gb/core/services/AccountService.java
+++ core-service/src/main/java/ru/gb/core/services/AccountService.java
@@ -17,12 +17,12 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
 
-    public Optional<Account> findByClientIdAndAccountNumber(Long clientId,String accountNum) {
-        return accountRepository.findByClientIdAccountByAccountNumber(clientId,accountNum);
+    public Optional<Account> findByClientUsernameAndAccountNumber(String username,String accountNum) {
+        return accountRepository.findByClientUsernameAccountByAccountNumber(username,accountNum);
     }
 
-    public List<Account> findAll(Long clientId) {
-        return accountRepository.findAllByClientId(clientId);
+    public List<Account> findAll(String username) {
+        return accountRepository.findAllByClientUsername(username);
     }
 
 
