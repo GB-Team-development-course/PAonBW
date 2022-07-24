51f4df0108937e346bb7fdbfa224e89e72fedb41
Sergei Afonin <s.afonin@bankffin.ru>
2022-07-24 15:17:20 +0300

    change AccountOperationService with using GlobalExceptionHandler

---
+++ core-service/src/main/java/ru/gb/core/exceptions/AccountNotFoundException.java
@@ -0,0 +1,25 @@
+package ru.gb.core.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountNotFoundException extends RuntimeException{
+
+    public AccountNotFoundException() {
+        super();
+    }
+
+    public AccountNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccountNotFoundException(final String message) {
+        super(message);
+    }
+
+    public AccountNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+}
--- core-service/src/main/java/ru/gb/core/exceptions/GlobalExceptionHandler.java
+++ core-service/src/main/java/ru/gb/core/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.gb.core.enums.ResponseCode;
+import ru.gb.core.response.Response;
+import ru.gb.core.response.ResponseFactory;
 
 @ControllerAdvice
 @Slf4j
@@ -21,6 +24,20 @@ public class GlobalExceptionHandler {
         log.error(e.getMessage(), e);
         return new ResponseEntity<>(new CoreError("FAILED_CREATE_CLIENT", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
-
+    @ExceptionHandler
+    public ResponseEntity<Response> failedFoundAccountException(AccountNotFoundException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ResponseFactory.errorResponse(ResponseCode.ACCOUNT_NOT_FOUND,e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<Response> failedCloseAccountException(CloseAccountException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ResponseFactory.errorResponse(ResponseCode.ACCOUNT_CLOSED_ERROR,e.getMessage()), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler
+    public ResponseEntity<Response> failedBlockAccountException(BlockAccountException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ResponseFactory.errorResponse(ResponseCode.ACCOUNT_BLOCK_ERROR,e.getMessage()), HttpStatus.CONFLICT);
+    }
 
 }
