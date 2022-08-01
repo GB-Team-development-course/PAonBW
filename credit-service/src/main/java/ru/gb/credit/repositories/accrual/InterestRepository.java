9814e945a8ae756c93dea76e1535e751bfba6631
Sergei Afonin <s.afonin@bankffin.ru>
2022-08-01 14:53:17 +0300

    delete duplicated classes

--- credit-service/src/main/java/ru/gb/credit/repositories/accrual/InterestRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.credit.repositories.accrual;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.gb.credit.entity.Interest;
-
-@Repository
-public interface InterestRepository extends JpaRepository<Interest, Long> {
-}
