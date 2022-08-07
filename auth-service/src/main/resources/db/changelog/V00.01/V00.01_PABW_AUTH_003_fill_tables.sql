1528bbf249c0d78ec0bf928b9e72c5c609700f90
Sergei Afonin <s.afonin@bankffin.ru>
2022-08-07 20:24:28 +0300

    Update gateway

--- auth-service/src/main/resources/db/changelog/V00.01/V00.01_PABW_AUTH_003_fill_tables.sql
+++ auth-service/src/main/resources/db/changelog/V00.01/V00.01_PABW_AUTH_003_fill_tables.sql
@@ -1,3 +1,12 @@
 insert into pabw_auth.roles (name)
 values ('ROLE_USER'),
        ('ROLE_ADMIN');
+
+
+insert into pabw_auth.users (username, password, email)
+values ('Jack', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'bob_johnson@gmail.com'),
+       ('John', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'john_johnson@gmail.com');
+
+insert into pabw_auth.users_roles (user_id, role_id)
+values (1, 1),
+       (2, 1);
